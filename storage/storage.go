d1b38f042cc37395531e982fd4b94460025b8864
abdurahmon-saydullayev <abdurahmonsaydullayev5@gmail.com>
2023-05-29 16:16:57 +0500

    updated

--- storage/storage.go
+++ storage/storage.go
@@ -1,9 +1,9 @@
 package storage
 
 import (
-	"context"
 	"Projects/store/user_service/genproto/user_service"
 	"Projects/store/user_service/models"
+	"context"
 )
 
 type StorageI interface {
@@ -13,15 +13,15 @@ type StorageI interface {
 
 type UserRepoI interface {
 	Create(context.Context, *user_service.CreateUser) (*user_service.UserPrimaryKey, error)
-	GetByPK(ctx context.Context, req *user_service.UserPrimaryKey) ( *user_service.User,  error)
+	GetByPK(ctx context.Context, req *user_service.UserPrimaryKey) (*user_service.User, error)
 	GetList(context.Context, *user_service.GetListUserRequest) (*user_service.GetListUserResponse, error)
 	Update(ctx context.Context, req *user_service.UpdateUser) (rowsAffected int64, err error)
 	UpdatePatch(ctx context.Context, req *models.UpdatePatchRequest) (rowsAffected int64, err error)
 	Delete(ctx context.Context, req *user_service.UserPrimaryKey) error
 
-	GetByPhoneNumber(context.Context, *user_service.UserPhoneNumberReq) (*user_service.User, error)
-
 	//otp
 	CreateOTP(context.Context, *user_service.CreateOTP) error
 	VerifyOTP(context.Context, *user_service.VerifyOTP) error
+
+	GetByPhoneNumber(context.Context, *user_service.UserPhoneNumberReq) (*user_service.User, error)
 }
